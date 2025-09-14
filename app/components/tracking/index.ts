c822aa4fe3050c67c273de95b1cd868aa8755a96
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-09-13 22:40:51 -0400

    refactor: remove deprecated tracking components and exports

--- app/components/tracking/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Tracking components barrel export
- */
-export { ProgressTrackingTab } from './ProgressTrackingTab';
-export { TrackingControls } from './TrackingControls';
-export { ProfessionProgressTable } from './ProfessionProgressTable';
