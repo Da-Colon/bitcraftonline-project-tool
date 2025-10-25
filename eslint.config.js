0049f7b26b160b6bb27bb8c1971c8e0ce2227fca
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-10-25 01:14:54 -0400

    chore: enhance ESLint configuration by adding global variables for browser and node environments

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import importPlugin from 'eslint-plugin-import';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
@@ -18,6 +19,10 @@ export default [
         ecmaFeatures: {
           jsx: true,
         },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
       },
     },
     plugins: {
